06dcfc5958adfef819a8eb78ccaf74f6b83628a6
hrridoyv2 <hrridoyv2@gmail.com>
2021-02-12 19:15:30 +0600

    deploy

---
+++ build/precache-manifest.8909a9a4e72acb6a70ac1b0825d2afd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a0fdf7f9fec7bb5ea03f9d858c6444",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6ffc860ae159f052198",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a6ffc860ae159f052198",
+    "url": "/static/js/2.217a641b.chunk.js"
+  },
+  {
+    "revision": "6e86f9c5a5fae07a01b8c01faf571f12",
+    "url": "/static/js/2.217a641b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23b82da200ac1adcc13",
+    "url": "/static/js/main.d13bad99.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  }
+]);
